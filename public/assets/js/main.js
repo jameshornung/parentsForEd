b54c22fc8bf38b72b4ff8ab32ab5f364a147184f
James Hornung <hornung.james@gmail.com>
2016-11-18 12:38:41 -0600

    style updates to navbar

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
-	console.log('attached');
 	twttr.widgets.createTimeline(
-  {
-    sourceType: "profile",
-    screenName: "austinparents"
-  },
-  document.getElementById("twitterFeed")
-);
+	  {
+	    sourceType: "profile",
+	    screenName: "austinparents"
+	  },
+	  document.getElementById("twitterFeed")
+	);
 });
