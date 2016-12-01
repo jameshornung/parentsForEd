2257b8d7b6a19bc9083799a2b9e21f298c790d77
James Hornung <hornung.james@gmail.com>
2016-12-01 08:48:12 -0600

    first two pages minimally presentable, added under costruction text to others

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -9,4 +9,28 @@ $(document).ready(function(){
 	);
 	// Hamburger Menu
 	$(".button-collapse").sideNav();
+
+	// Fixed Header (example)
+ //    $(".site-header").after('<div class="bumper"></div>');
+ //    $(window).scroll(function () {
+	//   if ($(document).scrollTop() > 1 ) {
+	//     $('.site-header').addClass('shrink');
+	//     $('.bumper').addClass('active');
+	//   } else {
+	//     $('.site-header').removeClass('shrink');
+	//     $('.bumper').removeClass('active');
+	//   }
+	// });
+
+	// $(".site-header").after('<div class="bumper"></div>');
+    $(window).scroll(function () {
+    	console.log(scrollTop);
+	  if ($(document).scrollTop() > 1 ) {
+	    $('.introBar').addClass('introBarShrink');
+	    // $('.bumper').addClass('active');
+	  } else {
+	    $('.introBar').removeClass('introBarShrink');
+	    // $('.bumper').removeClass('active');
+	  }
+	});
 });
